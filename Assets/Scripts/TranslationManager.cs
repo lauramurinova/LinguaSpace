a3a932df2b4448935c1b7d96130045af9ac41a86
lauramurinova <lauramurinova@gmail.com>
2024-04-24 16:36:37 -0300

    Google_Cloud_Translation_API: Finished

--- Assets/Scripts/TranslationManager.cs
+++ Assets/Scripts/TranslationManager.cs
@@ -1,59 +1,59 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Meta.WitAi.TTS.Utilities;
 using Meta.XR.MRUtilityKit;
 using Newtonsoft.Json.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
-using UnityEngine.UI;
 
 public enum TranslateLanguage
 {
     en = 0,
     es = 1,
     hu = 2,
-    sk = 3
+    fr = 3,
+    hr = 4
 }
 
 public class TranslationManager : MonoBehaviour
 {
-    [SerializeField] private TTSSpeaker _speaker;
+    [Header("MRUK")]
     [SerializeField] private MRUK _mruk;
+    [SerializeField] private MRUKAnchor.SceneLabels _sceneLabelsToShow;
     [SerializeField] private GameObject _labelPrefab;
+    
+    [Header("Translation")]
     [SerializeField] private TMP_Dropdown _languageDropdown;
     [SerializeField] private TextToSpeechManager _textToSpeechManager;
 
     private List<TranslateObject> _translateObjects = new List<TranslateObject>();
     private TranslateLanguage _currentLanguage = TranslateLanguage.en;
-    
-    private void Awake()
-    {
-        _mruk.SceneLoadedEvent.AddListener(LoadLabels);
-    }
 
+    private string _translationApiUrl = "https://translation.googleapis.com/language/translate/v2?key=";
+
+    /// <summary>
+    /// Loads all objects label within the current room.
+    /// Called by MRUK on scene loaded event.
+    /// </summary>
     public void LoadLabels()
     {
         var _mrukRoom = _mruk.GetCurrentRoom();
             
         foreach (var anchor in _mrukRoom.Anchors)
         {
-            var text = anchor.GetLabelsAsEnum().ToString();
-            var parts = text.Split('_');
-            text = parts[0];
-            var label = Instantiate(_labelPrefab, anchor.transform).GetComponent<TranslateObject>();
-            label.Initiate(text, _textToSpeechManager);
-            _translateObjects.Add(label);
+            if(!_sceneLabelsToShow.ToString().Contains(anchor.GetLabelsAsEnum().ToString())) continue;
+            
+            var labelObject = Instantiate(_labelPrefab, anchor.transform).GetComponent<TranslateObject>();
+            labelObject.Initiate(GetAnchorLabel(anchor), _textToSpeechManager);
+            _translateObjects.Add(labelObject);
         }
     }
 
-    public void Speak(string text)
-    {
-        _speaker.Speak(text);
-    }
-
+    /// <summary>
+    /// Changes the label language based on the provided desired language enum.
+    /// Uses events to check when translation finishes.
+    /// </summary>
     public void ChangeLanguage(TranslateLanguage desiredLanguage)
     {
         foreach (var translateObject in _translateObjects)
@@ -71,9 +71,13 @@ public class TranslationManager : MonoBehaviour
         _currentLanguage = desiredLanguage;
     }
 
+    /// <summary>
+    /// Handles sending REST API to Googles Translation API based on the text provided and original language and desired language enum.
+    /// On finishing the translation, it invokes the translate event.
+    /// </summary>
     public IEnumerator TranslateText(UnityEvent<string> translateEvent, string textToTranslate, TranslateLanguage originLanguage, TranslateLanguage desiredLanguage)
     {
-        string url = $"https://translation.googleapis.com/language/translate/v2?key={GetApiKey()}";
+        string url =  _translationApiUrl + GetApiKey();
         string jsonRequestBody = "{\"q\":\"" + textToTranslate + "\",\"source\":\"" + originLanguage + "\",\"target\":\"" + desiredLanguage + "\"}";
 
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonRequestBody);
@@ -87,9 +91,7 @@ public class TranslationManager : MonoBehaviour
 
         if (request.result == UnityWebRequest.Result.Success)
         {
-            string jsonResponse = request.downloadHandler.text;
-            var translatedText = ParseTranslatedText(jsonResponse);
-            translateEvent.Invoke(translatedText);
+            translateEvent.Invoke(ParseTranslatedText(request.downloadHandler.text));
         }
         else
         {
@@ -97,44 +99,79 @@ public class TranslationManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Returns enum of currently selected language.
+    /// </summary>
     public TranslateLanguage GetCurrentLanguage()
     {
         return _currentLanguage;
     }
     
+    /// <summary>
+    /// Called by the languages dropdown menu.
+    /// Handles the switch between value and language enum, assigns the currently selected language.
+    /// </summary>
     public void OnLanguageDropdownValueChanged(int index)
     {
         string selectedLanguage = _languageDropdown.options[index].text;
-        
-        if (selectedLanguage == "Spanish")
+
+        switch (selectedLanguage)
         {
-            ChangeLanguage(TranslateLanguage.es);
-        }
-        else if (selectedLanguage == "English")
-        {
-            ChangeLanguage(TranslateLanguage.en);
-        }
-        else if (selectedLanguage == "Hungarian")
-        {
-            ChangeLanguage(TranslateLanguage.hu);
-        }
-        else if (selectedLanguage == "Slovak")
-        {
-            ChangeLanguage(TranslateLanguage.sk);
+            case "Spanish":
+            {
+                ChangeLanguage(TranslateLanguage.es);
+                break;
+            }
+            case "English":
+            {
+                ChangeLanguage(TranslateLanguage.en);
+                break;
+            }
+            case "French":
+            {
+                ChangeLanguage(TranslateLanguage.fr);
+                break;
+            }
+            case "Hungarian":
+            {
+                ChangeLanguage(TranslateLanguage.hu);
+                break;
+            }
+            case "Croatian":
+            {
+                ChangeLanguage(TranslateLanguage.hr);
+                break;
+            }
         }
     }
 
+    /// <summary>
+    /// Gets the proper label from the MRUK Anchor.
+    /// </summary>
+    private string GetAnchorLabel(MRUKAnchor anchor)
+    {
+        var text = anchor.GetLabelsAsEnum().ToString();
+        if (text.Contains("ART"))
+        {
+            text = "PICTURE";
+        }
+        else
+        {
+            text = text.Split('_')[0];
+        }
 
+        return text;
+    }
+
+    /// <summary>
+    /// Handles Google Translation API response.
+    /// Returns the translation in string format.
+    /// </summary>
     private string ParseTranslatedText(string jsonResponse)
     {
         JObject json = JObject.Parse(jsonResponse);
         string translatedText = json["data"]["translations"][0]["translatedText"].Value<string>();
         return translatedText;
-    }
-
-    private void Start()
-    {
-        GetApiKey();
     }
 
     private string GetApiKey()
