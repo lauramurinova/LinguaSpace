20b3196dff3ea71138ab48ca86367016c15589ab
lauramurinova <lauramurinova@gmail.com>
2024-05-10 16:05:14 -0300

    Minor updates + updated Unity version to 2022.3.27

--- Assets/Scripts/RayInteractorVisual.cs
+++ Assets/Scripts/RayInteractorVisual.cs
@@ -3,32 +3,45 @@ using UnityEngine;
 
 public class RayInteractorVisual : MonoBehaviour
 {
-     [SerializeField] private RayInteractor _rayInteractor;
-     [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private RayInteractor _rayInteractor;
+    [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private float _minDistanceToShowLine = 1f;
+    [SerializeField] private float _minDistanceFromMenuToShowLine = 0.35f;
+    [SerializeField] private Transform _playerMenu;
 
-        protected virtual void Start()
+    protected virtual void Start()
+    {
+        this.AssertField(_rayInteractor, nameof(_rayInteractor));
+    }
+
+    private void LateUpdate()
+    {
+        if (_rayInteractor == null || _lineRenderer == null)
+            return;
+
+        if ((_rayInteractor.CollisionInfo.HasValue && Vector3.Distance(_rayInteractor.Origin, _rayInteractor.CollisionInfo.Value.Point) < _minDistanceToShowLine))
         {
-            this.AssertField(_rayInteractor, nameof(_rayInteractor));
+            _lineRenderer.positionCount = 0;
         }
-
-        private void LateUpdate()
+        else
         {
             _lineRenderer.positionCount = 2;
             _lineRenderer.SetPosition(0, _rayInteractor.Origin);
             _lineRenderer.SetPosition(1, _rayInteractor.End);
         }
+    }
 
-        #region Inject
+    #region Inject
 
-        public void InjectAllRayInteractorDebugGizmos(RayInteractor rayInteractor)
-        {
-            InjectRayInteractor(rayInteractor);
-        }
+    public void InjectAllRayInteractorDebugGizmos(RayInteractor rayInteractor)
+    {
+        InjectRayInteractor(rayInteractor);
+    }
 
-        public void InjectRayInteractor(RayInteractor rayInteractor)
-        {
-            _rayInteractor = rayInteractor;
-        }
+    public void InjectRayInteractor(RayInteractor rayInteractor)
+    {
+        _rayInteractor = rayInteractor;
+    }
 
-        #endregion
+    #endregion
 }
